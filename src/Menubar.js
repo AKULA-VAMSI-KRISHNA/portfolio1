bc4d4e214d0e57627887fce9405a5182b522e3ce
AKULA-VAMSI-KRISHNA <vamsikrishnaakula2@gmail.com>
2023-10-18 23:25:34 +0530

    modified

--- src/Menubar.js
+++ src/Menubar.js
@@ -8,7 +8,7 @@ function Menubar() {
         <img
           src="https://w7.pngwing.com/pngs/648/221/png-transparent-computer-icons-house-house-logo-angle-text-logo-thumbnail.png"
           alt=""
-          style={{ width: "1.8%" }}
+          style={{ width: "22px" }}
         />{" "}
         Home&nbsp;&nbsp;&nbsp;
       </NavLink>
@@ -20,7 +20,7 @@ function Menubar() {
         <img
           src="https://icons.veryicon.com/png/o/miscellaneous/cloud-his-icon/35-personal-information.png"
           alt=""
-          style={{ width: "2%" }}
+          style={{ width: "24px" }}
         />
         Personal Details&nbsp;&nbsp;&nbsp;
       </NavLink>
@@ -33,7 +33,7 @@ function Menubar() {
         <img
           src="https://www.freeiconspng.com/thumbs/education-png/education-png-1.png"
           alt=""
-          style={{ width: "2%" }}
+          style={{ width: "24px" }}
         />{" "}
         Educational Details&nbsp;&nbsp;&nbsp;
       </NavLink>
@@ -46,7 +46,7 @@ function Menubar() {
         <img
           src="https://banner2.cleanpng.com/20180601/piv/kisspng-project-infographic-management-ideas-5b10f6d55d3473.1939874115278384213818.jpg"
           alt=""
-          style={{ width: "2%" }}
+          style={{ width: "24px" }}
         />{" "}
         Projects&nbsp;&nbsp;&nbsp;
       </NavLink>
@@ -55,7 +55,7 @@ function Menubar() {
         <img
           src="https://www.vhv.rs/dpng/d/438-4381204_settings-vector-technical-skill-setting-icon-hd-png.png"
           alt=""
-          style={{ width: "2%" }}
+          style={{ width: "24px" }}
         />{" "}
         Technical Skills&nbsp;&nbsp;&nbsp;
       </NavLink>
@@ -68,7 +68,7 @@ function Menubar() {
         <img
           src="https://www.freeiconspng.com/thumbs/certificate-icon-png/certificate-icon-10.png"
           alt=""
-          style={{ width: "2%" }}
+          style={{ width: "24px" }}
         />{" "}
         Certifications&nbsp;&nbsp;&nbsp;
       </NavLink>
