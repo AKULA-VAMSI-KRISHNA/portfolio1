e6a4515cfc78b3369d84a149d1c0ae987078c695
AKULA-VAMSI-KRISHNA <vamsikrishnaakula2@gmail.com>
2023-10-18 15:21:49 +0530

    intermediate commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import Headder from "./Headder";
 import Menubar from "./Menubar";
 import "./App.css";
-import Home from './Components/Home';
-import Personal from './Components/Personal';
-import Educational from './Components/Educational';
-import Projects from './Components/Projects';
-import Skills from './Components/Skills';
-import Certificates from './Components/Certificates';
-import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Personal from "./Components/Personal";
+import Educational from "./Components/Educational";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Certificates from "./Components/Certificates";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Headder></Headder>
-      <Menubar></Menubar>
-      <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<App></App>}></Route> */}
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/Personal" element={<Personal></Personal>}></Route>
-        <Route path="/Educational" element={<Educational></Educational>}></Route>
-        <Route path="/Projects" element={<Projects></Projects>}></Route>
-        <Route path="/Skills" element={<Skills></Skills>}></Route>
-        <Route path="/Certificates" element={<Certificates></Certificates>}></Route>
-      </Routes>
-      </BrowserRouter>
-         
+      <Router>
+        <Headder></Headder>
+        <Menubar></Menubar>
+        <Routes>
+          {/* <Route path="/" element={<App></App>}></Route> */}
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/Personal" element={<Personal></Personal>}></Route>
+          <Route
+            path="/Educational"
+            element={<Educational></Educational>}
+          ></Route>
+          <Route path="/Projects" element={<Projects></Projects>}></Route>
+          <Route path="/Skills" element={<Skills></Skills>}></Route>
+          <Route
+            path="/Certificates"
+            element={<Certificates></Certificates>}
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Menubar.js
+++ src/Menubar.js
@@ -1,24 +1,78 @@
-import './Menubar.css';
-import {Link,NavLink} from "react-router-dom";
-import { BrowserRouter } from 'react-router-dom';
+import "./Menubar.css";
+import { NavLink } from "react-router-dom";
 function Menubar() {
-    return (
-            <div className="qwe">
-            <BrowserRouter>
-            <NavLink to='/' className="abc"style={{ textDecoration: 'none' }}> <img src="https://w7.pngwing.com/pngs/648/221/png-transparent-computer-icons-house-house-logo-angle-text-logo-thumbnail.png" alt="" style={{width:'1.8%'}} /> Home&nbsp;&nbsp;&nbsp;
-            </NavLink>
-            <NavLink to='/Personal' className="abc"style={{ textDecoration: 'none' }}><img src="https://icons.veryicon.com/png/o/miscellaneous/cloud-his-icon/35-personal-information.png" alt="" style={{width:'2%'}}/>Personal Details&nbsp;&nbsp;&nbsp;</NavLink>
-            <NavLink to='/Educational' className="abc"style={{ textDecoration: 'none' }}> <img src="https://www.freeiconspng.com/thumbs/education-png/education-png-1.png" alt="" style={{width:'2%'}} /> Educational Details&nbsp;&nbsp;&nbsp;
-            </NavLink>
-            <NavLink to='/Projects' className="abc"style={{ textDecoration: 'none' }}> <img src="https://banner2.cleanpng.com/20180601/piv/kisspng-project-infographic-management-ideas-5b10f6d55d3473.1939874115278384213818.jpg" alt="" style={{width:'2%'}}/> Projects&nbsp;&nbsp;&nbsp;
-            </NavLink>
-            <NavLink to='/Skills' className="abc" style={{ textDecoration: 'none' }}> <img src="https://www.vhv.rs/dpng/d/438-4381204_settings-vector-technical-skill-setting-icon-hd-png.png" alt="" style={{width:'2%'}}/> Technical Skills&nbsp;&nbsp;&nbsp;
-            </NavLink>
-            <NavLink to='/Certificates' className="abc" style={{ textDecoration: 'none' }}> <img src="https://www.freeiconspng.com/thumbs/certificate-icon-png/certificate-icon-10.png" alt="" style={{width:'2%'}}/> Certifications&nbsp;&nbsp;&nbsp;
-            </NavLink>
-            </BrowserRouter>
-            </div>
-        
-    )
+  return (
+    <div className="qwe">
+      <NavLink to="/" className="abc" style={{ textDecoration: "none" }}>
+        {" "}
+        <img
+          src="https://w7.pngwing.com/pngs/648/221/png-transparent-computer-icons-house-house-logo-angle-text-logo-thumbnail.png"
+          alt=""
+          style={{ width: "1.8%" }}
+        />{" "}
+        Home&nbsp;&nbsp;&nbsp;
+      </NavLink>
+      <NavLink
+        to="/Personal"
+        className="abc"
+        style={{ textDecoration: "none" }}
+      >
+        <img
+          src="https://icons.veryicon.com/png/o/miscellaneous/cloud-his-icon/35-personal-information.png"
+          alt=""
+          style={{ width: "2%" }}
+        />
+        Personal Details&nbsp;&nbsp;&nbsp;
+      </NavLink>
+      <NavLink
+        to="/Educational"
+        className="abc"
+        style={{ textDecoration: "none" }}
+      >
+        {" "}
+        <img
+          src="https://www.freeiconspng.com/thumbs/education-png/education-png-1.png"
+          alt=""
+          style={{ width: "2%" }}
+        />{" "}
+        Educational Details&nbsp;&nbsp;&nbsp;
+      </NavLink>
+      <NavLink
+        to="/Projects"
+        className="abc"
+        style={{ textDecoration: "none" }}
+      >
+        {" "}
+        <img
+          src="https://banner2.cleanpng.com/20180601/piv/kisspng-project-infographic-management-ideas-5b10f6d55d3473.1939874115278384213818.jpg"
+          alt=""
+          style={{ width: "2%" }}
+        />{" "}
+        Projects&nbsp;&nbsp;&nbsp;
+      </NavLink>
+      <NavLink to="/Skills" className="abc" style={{ textDecoration: "none" }}>
+        {" "}
+        <img
+          src="https://www.vhv.rs/dpng/d/438-4381204_settings-vector-technical-skill-setting-icon-hd-png.png"
+          alt=""
+          style={{ width: "2%" }}
+        />{" "}
+        Technical Skills&nbsp;&nbsp;&nbsp;
+      </NavLink>
+      <NavLink
+        to="/Certificates"
+        className="abc"
+        style={{ textDecoration: "none" }}
+      >
+        {" "}
+        <img
+          src="https://www.freeiconspng.com/thumbs/certificate-icon-png/certificate-icon-10.png"
+          alt=""
+          style={{ width: "2%" }}
+        />{" "}
+        Certifications&nbsp;&nbsp;&nbsp;
+      </NavLink>
+    </div>
+  );
 }
 export default Menubar;
